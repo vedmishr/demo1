272dd8b5e98f015b7a468ad73b651532daf8e2d3
vedmishr <vedant.mishra@capgemini.com>
2017-06-05 17:05:59 +0530

    Add files via upload

---
+++ Division.java
@@ -0,0 +1,14 @@
+package com.capgemini.code;
+
+public class Division {
+
+	public static void main(String[] args) {
+		
+		int a=10;
+		int b =100;
+		int c=b/a;
+		System.out.println(c);
+				
+	}
+
+}
---
+++ Multiplication.java
@@ -0,0 +1,14 @@
+package com.capgemini.code;
+
+public class Multiplication {
+
+	public static void main(String[] args) {
+		
+		int a=10;
+		int b =100;
+		int c=a*b;
+		System.out.println(c);
+				
+	}
+
+}
